647b29e2f6ac495d8d08a58502cfd6b28c8aebeb
Yashdarji31 <darji2730@gmail.com>
2026-03-29 01:21:39 +0530

    Improve admin panel

--- budgets/admin.py
+++ budgets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import Budget
-admin.site.register(Budget)
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ['user', 'category', 'amount', 'month', 'year']
+    list_filter = ['month', 'year']
+    search_fields = ['user__username']
--- transactions/admin.py
+++ transactions/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Transaction, Category
-admin.site.register(Transaction)
-admin.site.register(Category)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'amount', 'transaction_type', 'category', 'date']
+    list_filter = ['transaction_type', 'category', 'date']
+    search_fields = ['user__username', 'title']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category_type', 'user', 'is_default']
